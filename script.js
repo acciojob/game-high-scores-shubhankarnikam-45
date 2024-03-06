e71982a6357584fe56a2013c0aa6316b1c0415a1
shubhankarnikam-45 <120236922+shubhankarnikam-45@users.noreply.github.com>
2024-03-06 16:50:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,13 +3,88 @@ const nameInput = document.getElementById("name");
 const scoreInput = document.getElementById("score");
 const scores = document.getElementById("scores");
 
+let itemList = JSON.parse(localStorage.getItem("allEntries"));
+
+
+//content for table.
+
+console.log("top")
 // Save score to Local Storage
 function saveScore() {
   // complete the code here
-  showScores();
+
+	let itemList = JSON.parse(localStorage.getItem("allEntries"));
+	
+	//current name and score.
+	let currNameAndScore = {
+		"name":nameInput.value,
+		"score":scoreInput.value
+	}
+
+	
+	if(itemList === null)
+	{
+		//for very firsttime.
+		let array = [];
+		array.push(currNameAndScore);
+		localStorage.setItem("allEntries",JSON.stringify(array));
+		itemList = array;
+	}
+	else
+	{
+		console.log("second time")
+		itemList.push(currNameAndScore);
+		localStorage.setItem("allEntries",JSON.stringify(itemList));
+	}
+
+	//make again blank input.
+	nameInput.value ="";
+	scoreInput.value = "";
+	
+	showScores();
 }
 
 // Show scores in div
 function showScores() {
-  // complete the code
+
+	console.log("innnre");
+	scores.innerHTML ="";
+
+	let itemList = JSON.parse(localStorage.getItem("allEntries"));
+	if(itemList  === null)
+	{
+		//this means no score add yet.
+		console.log("s")
+		let h2Heading = document.createElement("h2");
+		h2Heading.innerText = "No scores yet";
+		scores.appendChild(h2Heading);
+	}
+	else
+	{
+		
+		let table = document.createElement("table");	
+		let th1 = document.createElement("th");	
+		let th2 = document.createElement("th");	
+		th1.innerText = "Name";
+		th2.innerText = "Score";
+		table.appendChild(th1);
+		table.appendChild(th2);
+		console.log("itemlist",itemList)
+		//we using map to iterate over the name and score.
+		itemList.forEach((item)=>{
+			let tr = document.createElement("tr");	
+			let tc1 = document.createElement("th");
+			tc1.innerText = item.name;
+			let tc2 = document.createElement("th");
+			tc2.innerText = item.score;
+			tr.appendChild(tc1);
+			tr.appendChild(tc2);
+			table.appendChild(tr)
+		// 	//craete the row.
+			console.log("item",item)
+		})
+		
+		scores.appendChild(table);
+	}
+	
 }
